f771fcc1a9479580ee4beb101c4a1d5097d83d80
vlad polevoi <vlad.polewoi1@gmail.com>
2025-12-01 12:36:07 +0200

    :sparkles: Use CRXJS starter project

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite'
+import path from 'node:path'
+import { crx } from '@crxjs/vite-plugin'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import zip from 'vite-plugin-zip-pack'
+import manifest from './manifest.config.js'
+import { name, version } from './package.json'
 
-// https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': `${path.resolve(__dirname, 'src')}`,
+    },
+  },
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
+    react(),
+    crx({ manifest }),
+    zip({ outDir: 'release', outFileName: `crx-${name}-${version}.zip` }),
   ],
+  server: {
+    cors: {
+      origin: [
+        /chrome-extension:\/\//,
+      ],
+    },
+  },
 })
