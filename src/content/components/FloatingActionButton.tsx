4b0bf463b4dbd27569df11d6038734e570db4a6b
vlad polevoi <vlad.polewoi1@gmail.com>
2025-12-03 02:28:26 +0200

    :art: Fix responsive styles

--- src/content/components/FloatingActionButton.tsx
+++ src/content/components/FloatingActionButton.tsx
@@ -6,7 +6,7 @@ interface FloatingActionButtonProps {
 
 export function FloatingActionButton({ onClick }: FloatingActionButtonProps) {
   return (
-    <div className="fixed bottom-6 right-6 z-[10000]">
+    <div className="fixed bottom-24 right-6 z-[10000]">
       <button
         onClick={onClick}
         className="relative flex h-14 w-14 items-center justify-center rounded-full bg-primary shadow-lg hover:shadow-xl transition-all hover:scale-105"
